d8b647ea7545da3f8e90f23a333b16ae7b185f9d
Gabriela Wieliczko <gawieliczko@gmail.com>
2020-11-20 14:43:33 +0100

    test

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use('/auth', require('./routes/auth')
 //Serve static assests if in production
 if(process.env.NODE_ENV==='production'){
     //Set static folder
-    app.use(express.static(path_join(__dirname, '/client/build')));
+    app.use(express.static('/client/build'));
     // app.get('*',(req,res)=>{
     //     res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     // })
